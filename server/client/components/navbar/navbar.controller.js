2661521c0308287d14296997bdc98152b6e4d90c
Alexander Ulanouski <alexander.ulanouski@a1u.cc>
2015-10-03 01:26:44 +0200

    added generated server project

---
+++ server/client/components/navbar/navbar.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('serverApp')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.menu = [{
+      'title': 'Home',
+      'state': 'main'
+    }];
+
+    $scope.isCollapsed = true;
+  });
