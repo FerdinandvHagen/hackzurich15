c93979d4ef29f389e25de392cb6bf78a17547b1e
Alexander Ulanouski <alexander.ulanouski@a1u.cc>
2015-10-03 02:54:10 +0200

    uart tx

--- client/esp_iot_rtos_sdk/wiloc/user/user_main.c
+++ client/esp_iot_rtos_sdk/wiloc/user/user_main.c
@@ -36,14 +36,21 @@ SOFTWARE.
 
 #include "user_config.h"
 #include "gpio.h"
+#include "itstr.h"
+
+// this field depends on total count of our nodes
+#define MAX_INGORE_MACS 16
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 #define DBG(str, ...) printf(str "\n", ##__VA_ARGS__)
 
-#define printmac(buf, i) printf("\t%02X:%02X:%02X:%02X:%02X:%02X", buf[i+0], buf[i+1], buf[i+2], buf[i+3], buf[i+4], buf[i+5])
+#define printmac(buf, i) printf("%02X:%02X:%02X:%02X:%02X:%02X", buf[i+0], buf[i+1], buf[i+2], buf[i+3], buf[i+4], buf[i+5])
 
 xTimerHandle timer;
+
+unsigned char ignored_macs[MAX_INGORE_MACS][6];
+int ignored_macs_count = 0;
 
 void promisc_cb(uint8 *buf, uint16 len) {
     gpio_signal(1);
@@ -60,10 +67,10 @@ void promisc_cb(uint8 *buf, uint16 len) {
         }
         printf("\n");
     } else if (len == 12) {
-        struct RxControl *rx_ctrl = (struct RxControl*) buf;
-        printf("rx rssi:%d rate: %d ch:%d \n",rx_ctrl->rssi, rx_ctrl->rate, rx_ctrl->channel);
+        struct RxControl *rx_ctrl = (struct RxControl *) buf;
+        printf("rx rssi:%d rate: %d ch:%d \n", rx_ctrl->rssi, rx_ctrl->rate, rx_ctrl->channel);
     } else {
-        struct sniffer_buf *sb = (struct sniffer_buf*) buf;
+        struct sniffer_buf *sb = (struct sniffer_buf *) buf;
         printf("p rssi:%d rate: %d ch:%d \n", sb->rx_ctrl.rssi, sb->rx_ctrl.rate, sb->rx_ctrl.channel);
         printf("cnt:%d head: ", sb->cnt);
         int i;
@@ -83,6 +90,50 @@ void promisc_cb(uint8 *buf, uint16 len) {
     gpio_signal(1);
 }
 
+/**
+ * UART TX packet structure:
+ * | packet count : 8 | timestamp : 8 * 8 | rssi : 8 * 2 | end : 8 |
+ */
+
+unsigned char packet_cnt = 1;
+
+typedef union {
+    struct {
+        unsigned char count;
+        unsigned char timestamp[8];
+        unsigned char rssi[2];
+        unsigned char mac[6][2];
+    };
+    unsigned char bytes[1 + 8 + 2 + 12];
+} uart_tx_struct;
+
+void uart_tx(int timestamp, char rssi, char *mac) {
+
+    uart_tx_struct tx;
+
+    tx.count = packet_cnt;
+    itstr((char *) tx.timestamp, sizeof(tx.timestamp), timestamp, 16, sizeof(tx.timestamp));
+    itstr((char *) tx.rssi, sizeof(tx.rssi), rssi, 16, sizeof(tx.rssi));
+
+    int i;
+    for (i = 0; i < 6; i++) {
+        itstr((char *) tx.mac[i], 2, mac[i], 16, 2);
+    }
+
+    // send bytes constructed so far and termination byte
+
+    for (i = 0; i < sizeof(tx.bytes); i++) {
+        uart_tx_one_char(UART, tx.bytes[i]);
+    }
+
+    uart_tx_one_char(UART, 0);
+
+    packet_cnt++;
+    if (!packet_cnt) {
+        packet_cnt = 1;
+    }
+}
+
 void channelHop(xTimerHandle pxTimer) {
     // 1 - 13 channel hopping
     uint8 new_channel = 10; //wifi_get_channel() % 13 + 1;
@@ -95,9 +146,26 @@ void main_task(void *pvParameters) {
 
     gpio_init();
 
-    wifi_promiscuous_enable(1);
+    // disable prom mode temporarily
+    //wifi_promiscuous_enable(1);
 
     vTaskSuspend(NULL);
+}
+
+// UART RX parsing
+
+typedef enum {
+    WAITING, CTRL_BYTE, DATA, END
+};
+
+void uart_rx(int len) {
+
+    int i;
+
+    for (i = 0; i < len; i++) {
+        printf("rx:%d", uart_rx_one_char(UART));
+    }
+
 }
 
 /******************************************************************************
@@ -108,11 +176,23 @@ void main_task(void *pvParameters) {
 *******************************************************************************/
 void user_init(void) {
     system_update_cpu_freq(160);
-    uart_init_new(BAUD, NULL);
+    uart_init_new(BAUD, uart_rx);
 
     printf("setting cpu freq to 160 MHz\n");
 
     printf("SDK version:%s\n", system_get_sdk_version());
+
+    unsigned char mac[6];
+
+    wifi_get_macaddr(STATION_IF, mac);
+    printf("MAC-STA:");
+    printmac(mac, 0);
+    printf("\n");
+
+    wifi_get_macaddr(SOFTAP_IF, mac);
+    printf("MAC- AP:");
+    printmac(mac, 0);
+    printf("\n");
 
 #ifdef DOUBLE_CLK_FREQ
     system_update_cpu_freq(160);
@@ -136,6 +216,7 @@ void user_init(void) {
 
     timer = xTimerCreate("channel_hopping", CHANNEL_HOP_INTERVAL / portTICK_RATE_MS, pdTRUE, (void *) 0, channelHop);
 
+#ifdef CHANNEL_HOP_ENABLED
     if (timer) {
         if (xTimerStart(timer, 0) != pdPASS) {
             DBG(" ---- failed to start timer");
@@ -143,6 +224,7 @@ void user_init(void) {
     } else {
         DBG(" ---- failed to create timer");
     }
+#endif
 
     DBG(" ---- done");
 
