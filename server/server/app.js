07d2c8b16d0c70d0dff69332bdef3fc22b672307
ferdinandvhagen <webmaster@vonhagen.eu>
2015-10-03 06:33:42 +0200

    updated app.js

--- server/server/app.js
+++ server/server/app.js
@@ -3,23 +3,31 @@ var app = express();
 
 var net = require('net');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/hackzurich15');
+
+var prefix = '';
+
 var socketServer = net.createServer(function (socket) {
 	socket.on('end', function () {
 		console.log('client disconnected');
 	});
-
+	
+	console.log("Client connected :)  " + socket.remoteAddress);
 	socket.on('data', function (data) {
-		chunks = String(data).split("\0");
-		console.log('Result: ' + String(data));
-		console.log('Chunk size: ' + chunks.length + ' hh ' + chunks[0]);
-
-		for (i = 0; i < chunks.length; ++i) {
-			if (chunks[i].length == 31) {
-				console.log('Found: ' + chunks[i]);
-			}
-		}
+		console.log(String(data));
 	});
 });
+
+function decode(chunk) {
+	//First get the client ID
+	character = chunk.split('');
+
+	for (i = 0; i < 31; ++i) {
+		console.log(i + ' ' + character[i]);
+	}
+}
+
 
 socketServer.listen(3003, function () {
 	console.log('Socket Server is now bound');
