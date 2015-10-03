eba836b4989a5fd2ca2fb873d13ad3a1177e6a93
ferdinandvhagen <webmaster@vonhagen.eu>
2015-10-03 18:10:36 +0200

    Dumping everything into mongo

--- server/server/app.js
+++ server/server/app.js
@@ -3,33 +3,94 @@ var app = express();
 
 var net = require('net');
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/hackzurich15');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
 
-var socketServer = net.createServer(function (socket) {
-	socket.on('end', function () {
-		console.log('client disconnected');
+var url = 'mongodb://localhost:27017/hackzurich15';
+MongoClient.connect(url, function (err, db) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server.");
+	db.close();
+});
+
+var insertDocument = function (db, callback) {
+	db.collection('restaurants').insertOne({
+		"address": {
+			"street": "2 Avenue",
+			"zipcode": "10075",
+			"building": "1480",
+			"coord": [-73.9557413, 40.7720266]
+		},
+		"borough": "Manhattan",
+		"cuisine": "Italian",
+		"grades": [
+			{
+				"date": new Date("2014-10-01T00:00:00Z"),
+				"grade": "A",
+				"score": 11
+			},
+			{
+				"date": new Date("2014-01-16T00:00:00Z"),
+				"grade": "B",
+				"score": 17
+			}
+		],
+		"name": "Vella",
+		"restaurant_id": "41704620"
+	}, function (err, result) {
+		assert.equal(err, null);
+		console.log("Inserted a document into the restaurants collection.");
+		callback(result);
+	});
+};
+
+MongoClient.connect(url, function (err, db) {
+	assert.equal(null, err);
+	var socketServer = net.createServer(function (socket) {
+		socket.on('end', function () {
+			console.log('client disconnected');
+		});
+
+		socket.on('timeout', function () {
+			console.log('Closing socket to ' + socket.remoteAddress + ' due to a timeout');
+			socket.destroy();
+		})
+
+		console.log("Client connected :)  " + socket.remoteAddress);
+		socket.setTimeout(60000);
+		
+		socket.on('data', function (data) {
+			chunks = String(data).split('\0');
+			for (i = 0; i < chunks.length; ++i) {
+				if (chunks[i].length == 31) {
+					//ignore first character
+					console.log('ID: ' + chunks[i].charCodeAt(2));
+					decode(chunks[i].substring(3, chunks[i].length));
+					db.collection('dump').insertOne({
+						"data": data,
+						"timestamp": process.hrtime()
+					}, function (err, result) {
+						assert.equal(err.null);
+					});
+				}
+			}
+		});
 	});
 
-	console.log("Client connected :)  " + socket.remoteAddress);
-	socket.on('data', function (data) {
-		chunks = String(data).split('\0');
-		for (i = 0; i < chunks.length; ++i) {
-			if (chunks[i].length == 31) {
-				//ignore first character
-				console.log('ID: ' + chunks[i].charCodeAt(2));
-				decode(chunks[i].substring(3, chunks[i].length));
-			}
-		}
+	socketServer.listen(3003, function () {
+		console.log('Socket Server is now bound');
 	});
 });
+
+
 
 function decode(chunk) {
 	console.log(chunk);
 	//First decode the Type
 	subtype = parseInt(chunk.charAt(24), 16);
 	maintype = parseInt(chunk.charAt(25), 16);
-	
+
 	switch (maintype) {
 		case 0:
 			console.log('Type: Management');
@@ -228,18 +289,3 @@ function decode(chunk) {
 	//console.log('Timestamp: ' + timestamp);
 }
 
-
-socketServer.listen(3003, function () {
-	console.log('Socket Server is now bound');
-});
-
-app.get('/', function (req, res) {
-	res.send('Hello World!');
-});
-
-var server = app.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log('Example app listening at http://%s:%s', host, port);
-});
